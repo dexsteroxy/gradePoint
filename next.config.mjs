efa7c56e005367dc1de009b39d6cd22bb5f20c05
Amadi sixtus <dexsteroxy.1234@gmail.com>
2024-08-09 16:09:39 +0100

    debuging it

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+
+// export default nextConfig;
+
+
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  // other configurations...
+}
+
+export default nextConfig;
